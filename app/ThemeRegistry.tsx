15952ea469ab094918e87e643102d0156e0d6078
aleksandra <cislowski.aleksandra@gmail.com>
2026-03-28 15:28:09 +0100

    feat: add ThemeRegistry component for enhanced theming support

---
+++ app/ThemeRegistry.tsx
@@ -0,0 +1,78 @@
+'use client';
+
+import * as React from 'react';
+import createCache from '@emotion/cache';
+import { CacheProvider, type EmotionCache } from '@emotion/react';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { useServerInsertedHTML } from 'next/navigation';
+import darkTheme from '../theme/darkTheme';
+
+type ThemeRegistryProps = {
+  children: React.ReactNode;
+};
+
+type EmotionCacheWithFlush = EmotionCache & {
+  flush: () => string[];
+};
+
+export default function ThemeRegistry({ children }: ThemeRegistryProps) {
+  const [{ cache, flush }] = React.useState(() => {
+    const cache = createCache({
+      key: 'mui',
+      prepend: true,
+    }) as EmotionCacheWithFlush;
+
+    cache.compat = true;
+
+    const prevInsert = cache.insert;
+    let inserted: string[] = [];
+
+    cache.insert = (...args) => {
+      const serialized = args[1];
+
+      if (cache.inserted[serialized.name] === undefined) {
+        inserted.push(serialized.name);
+      }
+
+      return prevInsert(...args);
+    };
+
+    const flush = () => {
+      const prevInserted = inserted;
+      inserted = [];
+      return prevInserted;
+    };
+
+    return { cache, flush };
+  });
+
+  useServerInsertedHTML(() => {
+    const names = flush();
+
+    if (names.length === 0) {
+      return null;
+    }
+
+    let styles = '';
+
+    for (const name of names) {
+      styles += cache.inserted[name];
+    }
+
+    return (
+      <style
+        data-emotion={`${cache.key} ${names.join(' ')}`}
+        dangerouslySetInnerHTML={{ __html: styles }}
+      />
+    );
+  });
+
+  return (
+    <CacheProvider value={cache}>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
